86941faba798bf4882ec8ae444bc013cc74585f0
José Francisco Riffo <jfriffoa@gmail.com>
2020-06-10 10:59:29 -0400

    Spawn Different Platforms

--- js/game.js
+++ js/game.js
@@ -1,4 +1,10 @@
 class GameScene extends Phaser.Scene {
+    cloudTypes = {
+        KEY: "type",
+        NORMAL: 1,
+        BAD: 2
+    }
+    
     constructor() {
         super({ key: 'Game', active: false });
     }
@@ -6,12 +12,18 @@ class GameScene extends Phaser.Scene {
     init() {
         this.jumpVelocity = -1500; // Negative is up
         this.dead = false;
+        this.score = 0;
     }
 
     preload() {
         this.load.image('player-idle', 'assets/peluchin-idle.png');
         this.load.image('player-jump', 'assets/peluchin-jumping.png');
-        this.load.image('platform', 'assets/nube1-1.png');
+        this.load.image('platform1', 'assets/platforms/nube1-1.png');
+        this.load.image('platform2', 'assets/platforms/nube2-1.png');
+        this.load.image('platform3', 'assets/platforms/nube3-1.png');
+        this.load.image('platform4', 'assets/platforms/nube-mala1-1.png');
+        this.load.image('platform5', 'assets/platforms/nube-mala2-1.png');
+        this.load.image('platform6', 'assets/platforms/nube-mala3-1.png');
     }
 
     create() {
@@ -33,11 +45,13 @@ class GameScene extends Phaser.Scene {
         //Add Platforms
         this.platforms = this.physics.add.staticGroup();
         for (let i = 0; i < 3; i++){
+            let style = Phaser.Math.Between(1, 3);
             this.lastPlatformSpawn = this.cameras.main.displayHeight - 500 * (i + 1);
-            let plat = this.platforms.create(this.cameras.main.centerX, this.lastPlatformSpawn, 'platform');
+            let plat = this.platforms.create(this.cameras.main.centerX, this.lastPlatformSpawn, 'platform' + style);
             plat.setDisplaySize(this.cameras.main.displayWidth * 0.3, 100);
             plat.setScale(plat.scaleX, plat.scaleX);
             plat.refreshBody();
+            plat.setData(this.cloudTypes.KEY, this.cloudTypes.NORMAL);
 
             let _x = plat.displayWidth * 0.16;
             let _y = plat.displayHeight * 0.19;
@@ -66,6 +80,12 @@ class GameScene extends Phaser.Scene {
 
     // Create UI
     createUI(){
+        // Add score text
+        this.scoreText = this.add.bitmapText(this.cameras.main.displayWidth * 0.05, 10, 'set-fire', "Score: " + Math.ceil(this.score));
+        this.scoreText.setTintFill(0x000000);
+        this.scoreText.setFontSize(this.cameras.main.displayHeight * 0.05);
+        this.scoreText.setDepth(10);
+
         // Add Buttons
         this.buttons = [];
         for (let i = 0; i < 2; i++){
@@ -82,6 +102,17 @@ class GameScene extends Phaser.Scene {
 
         this.buttons[0].onPressed((time, delta) => { this.movePlayer(-1); });
         this.buttons[1].onPressed((time, delta) => { this.movePlayer(1); });
+    }
+
+    moveUI() {
+        // Move the Text
+        this.scoreText.y = this.cameras.main.scrollY + 10;
+
+        // Move the buttons
+        this.buttons.forEach((item, _) => { item.y = this.cameras.main.scrollY; });
+
+        // Move the bg FOR NOW
+        this.background.y = this.cameras.main.scrollY;
     }
 
     update(time, delta){
@@ -110,19 +141,17 @@ class GameScene extends Phaser.Scene {
             this.lastUpScroll = this.player.y;
             // Move the Camera
             this.cameras.main.scrollY = this.player.y - this.cameras.main.displayHeight/2;
-            // Move the buttons as well
-            this.buttons.forEach((item, _) => { item.y = this.cameras.main.scrollY; });
-            // Move the bg FOR NOW
-            this.background.y = this.cameras.main.scrollY;
+            this.moveUI();
+
+            // Update Score
+            this.score = this.score + (this.lastUpScroll - this.cameras.main.scrollY) / 10000;
+            this.scoreText.setText("Score: " + Math.ceil(this.score));
         } else if (this.player.y > this.cameras.main.centerY) { // Going Down
             let downScroll = Math.abs(this.lastUpScroll - this.player.y);
             if (downScroll < this.deltaScroll) {
                 // Move the Camera
                 this.cameras.main.scrollY = this.player.y - this.cameras.main.displayHeight/2;
-                // Move the buttons as well
-                this.buttons.forEach((item, _) => { item.y = this.cameras.main.scrollY; });
-                // Move the bg FOR NOW
-                this.background.y = this.cameras.main.scrollY;
+                this.moveUI();
             }
         }
 
@@ -158,15 +187,22 @@ class GameScene extends Phaser.Scene {
         }
 
         player.setVelocityY(this.jumpVelocity);
+
+        if (platform.getData(this.cloudTypes.KEY) == this.cloudTypes.BAD) {
+            platform.destroy();
+        }
     }
 
     spawnCloud(){
-        this.lastPlatformSpawn = this.lastPlatformSpawn - Phaser.Math.RND.integerInRange(100, 600);
-        let platX = Phaser.Math.RND.integerInRange(0, this.cameras.main.displayWidth * 0.67) + this.cameras.main.displayWidth * 0.33/2;
-        let plat = this.platforms.create(platX, this.lastPlatformSpawn, 'platform');
+        let type = Phaser.Math.Between(1, 2);
+        let style = Phaser.Math.Between(1, 3);
+        this.lastPlatformSpawn = this.lastPlatformSpawn - Phaser.Math.Between(100, 600);
+        let platX = Phaser.Math.Between(0, this.cameras.main.displayWidth * 0.67) + this.cameras.main.displayWidth * 0.33/2;
+        let plat = this.platforms.create(platX, this.lastPlatformSpawn, 'platform' + (type * style));
         plat.setDisplaySize(this.cameras.main.displayWidth * 0.3, 100);
         plat.setScale(plat.scaleX, plat.scaleX);
         plat.refreshBody();
+        plat.setData(this.cloudTypes.KEY, type);
 
         let _x = plat.displayWidth * 0.16;
         let _y = plat.displayHeight * 0.19;
@@ -199,6 +235,10 @@ class GameScene extends Phaser.Scene {
     }
 
     showGameOverScreen(){
+        // Hide Prev UI
+        this.scoreText.setVisible(false);
+
+        // Draw Game Over Screen
         let y = this.cameras.main.scrollY + this.cameras.main.centerY;
         let logo = new ImageButton(this, this.cameras.main.centerX, y, 'logo').setOrigin(0.5, 0.5);
         logo.setDisplaySize(this.cameras.main.displayWidth - 100, 100);
